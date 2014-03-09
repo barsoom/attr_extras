bf4a922e8359cf371a6eb064ef4ff88a7094ca8c
Henrik Nyh <henrik@nyh.se>
2014-03-09 10:49:59 +0100

    Define minimal classes within each spec, not globally at top.

--- spec/attr_extras_spec.rb
+++ spec/attr_extras_spec.rb
@@ -2,54 +2,47 @@ require "minitest/autorun"
 require "minitest/pride"
 require "attr_extras"
 
-class Example
-  attr_initialize :foo, :bar
-  attr_private :foo, :bar
-end
-
-class PattrExample
-  pattr_initialize :foo, :bar
-end
-
-class QueryExample
-  attr_id_query :baz?, :boink?
-  attr_query :flurp?
-  attr_accessor :baz_id, :flurp
-end
-
-class MethodObjectExample
-  method_object :fooable?,
-    :foo
-
-  def fooable?
-    foo
-  end
-end
-
-class ExampleWithHash
-  pattr_initialize :foo, [:bar, :baz]
-end
-
 describe Object, ".method_object" do
+  let(:klass) do
+    Class.new do
+      method_object :fooable?,
+        :foo
+
+      def fooable?
+        foo
+      end
+    end
+  end
+
   it "creates a class method that instantiates and runs that instance method" do
-    assert MethodObjectExample.fooable?(true)
-    refute MethodObjectExample.fooable?(false)
+    assert klass.fooable?(true)
+    refute klass.fooable?(false)
   end
 end
 
 describe Object, ".attr_initialize" do
+  let(:klass) do
+    Class.new do
+      attr_initialize :foo, :bar
+    end
+  end
+
   it "creates an initializer setting those instance variables" do
-    example = Example.new("Foo", "Bar")
+    example = klass.new("Foo", "Bar")
     example.instance_variable_get("@foo").must_equal "Foo"
     example.instance_variable_get("@bar").must_equal "Bar"
   end
 
   it "requires all arguments" do
-    lambda { Example.new("Foo") }.must_raise ArgumentError
+    lambda { klass.new("Foo") }.must_raise ArgumentError
   end
 
   it "can set ivars from a hash" do
-    example = ExampleWithHash.new("Foo", :bar => "Bar", :baz => "Baz")
+    klass = Class.new do
+      pattr_initialize :foo, [:bar, :baz]
+    end
+
+    example = klass.new("Foo", :bar => "Bar", :baz => "Baz")
     example.instance_variable_get("@foo").must_equal "Foo"
     example.instance_variable_get("@bar").must_equal "Bar"
     example.instance_variable_get("@baz").must_equal "Baz"
@@ -57,8 +50,16 @@ describe Object, ".attr_initialize" do
 end
 
 describe Object, ".attr_private" do
+  let(:klass) do
+    Class.new do
+      attr_private :foo, :bar
+    end
+  end
+
   it "creates private readers" do
-    example = Example.new("Foo", "Bar")
+    example = klass.new
+    example.instance_variable_set("@foo", "Foo")
+    example.instance_variable_set("@bar", "Bar")
     example.send(:foo).must_equal "Foo"
     example.send(:bar).must_equal "Bar"
     lambda { example.foo }.must_raise NoMethodError
@@ -67,20 +68,34 @@ end
 
 describe Object, ".pattr_initialize" do
   it "creates both initializer and private readers" do
-    example = PattrExample.new("Foo", "Bar")
+    klass = Class.new do
+      pattr_initialize :foo, :bar
+    end
+
+    example = klass.new("Foo", "Bar")
     example.send(:foo).must_equal "Foo"
   end
 
   it "works with hash ivars" do
-    example = ExampleWithHash.new("Foo", :bar => "Bar")
+    klass = Class.new do
+      pattr_initialize :foo, [:bar, :baz]
+    end
+
+    example = klass.new("Foo", :bar => "Bar")
     example.send(:bar).must_equal "Bar"
   end
 end
 
 describe Object, ".attr_id_query" do
   it "creates id query methods" do
-    example = QueryExample.new
+    klass = Class.new do
+      attr_id_query :baz?, :boink?
+      attr_accessor :baz_id
+    end
+
+    example = klass.new
     refute example.baz?
+
     example.baz_id = 123
     assert example.baz?
   end
@@ -92,7 +107,12 @@ end
 
 describe Object, ".attr_query" do
   it "creates attribute query methods" do
-    example = QueryExample.new
+    klass = Class.new do
+      attr_query :flurp?
+      attr_accessor :flurp
+    end
+
+    example = klass.new
     refute example.flurp?
     example.flurp = "!"
     assert example.flurp?
