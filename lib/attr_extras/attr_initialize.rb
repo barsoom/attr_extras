d324d28b49b929c1d5feb01f1b5d44c0f696a2e6
Henrik Nyh <henrik@nyh.se>
2014-03-15 09:37:08 +0100

    AttrInitialize: extract set_ivar_from_hash

--- lib/attr_extras/attr_initialize.rb
+++ lib/attr_extras/attr_initialize.rb
@@ -11,24 +11,17 @@ class AttrInitialize
     # things available via local variables.
     names = @names
     validate_arity = method(:validate_arity)
+    set_ivar_from_hash = method(:set_ivar_from_hash)
 
     klass.send(:define_method, :initialize) do |*values|
       validate_arity.call(values.length)
 
       names.zip(values).each do |name_or_names, value|
         if name_or_names.is_a?(Array)
-          value ||= {}
+          hash = value || {}
 
           name_or_names.each do |name|
-            if name.to_s.end_with?("!")
-              actual_name = name.to_s.chop.to_sym
-              actual_value = value.fetch(actual_name)
-            else
-              actual_name = name
-              actual_value = value[name]
-            end
-
-            instance_variable_set("@#{actual_name}", actual_value)
+            set_ivar_from_hash.call(self, name, hash)
           end
         else
           name = name_or_names
@@ -42,11 +35,23 @@ class AttrInitialize
 
   def validate_arity(provided_arity)
     arity_without_hashes = names.count { |name| not name.is_a?(Array) }
-    arity_with_hashes = names.length
+    arity_with_hashes    = names.length
 
     unless (arity_without_hashes..arity_with_hashes).include?(provided_arity)
       arity_range = [arity_without_hashes, arity_with_hashes].uniq.join("..")
       raise ArgumentError, "wrong number of arguments (#{provided_arity} for #{arity_range})"
     end
+  end
+
+  def set_ivar_from_hash(instance, name, hash)
+    if name.to_s.end_with?("!")
+      actual_name = name.to_s.chop.to_sym
+      value = hash.fetch(actual_name)
+    else
+      actual_name = name
+      value = hash[name]
+    end
+
+    instance.instance_variable_set("@#{actual_name}", value)
   end
 end
